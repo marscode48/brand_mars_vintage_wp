5ec03186e6809fdb46b33e6b5a3f917f0d22c82a
Adachi Masakazu <marscode48@gmail.com>
2022-05-10 17:36:33 +0900

    footer.phpを作成

---
+++ wp-content/themes/mv/footer.php
@@ -0,0 +1,23 @@
+<footer id="footer" class="footer appear up">
+      <div class="footer__inner">
+        <div class="footer__logo item">
+          <a href="#"><img src="images/logo.png" alt="Mars Vintage">
+          </a>
+        </div>
+        <nav class="footer__nav">
+          <ul class="footer__ul">
+            <li class="item"><a href="products.html">PRODUCTS</a></li>
+            <li class="item"><a href="about.html">ABOUT</a></li>
+            <li class="item"><a href="company.html">COMPANY</a></li>
+            <li class="item"><a href="mailto:xxxxx@xxx.xxx.com?subject=お問い合わせ">CONTACT</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="footer__copyright item">
+        &copy; <?php echo bloginfo('name'); ?>
+      </div>
+    </footer>
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
