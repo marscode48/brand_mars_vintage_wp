a5680d1639a5b8124d47d89a5e091ae7948b438f
Adachi Masakazu <marscode48@gmail.com>
2022-05-12 17:20:06 +0900

    ｍain-visualとnew-arrivalを作成

--- wp-content/themes/mv/index.php
+++ wp-content/themes/mv/index.php
@@ -1,3 +1,165 @@
 <?php get_header(); ?>
 
+<main class="main">
+  <!-- メインビジュアル -->
+  <div class="mainvisual">
+    <img src="<?php echo esc_url(get_theme_file_uri('images/mainvisual1.jpg')); ?>" alt="" class="fade-slide">
+    <img src="<?php echo esc_url(get_theme_file_uri('images/mainvisual2.jpg')); ?>" alt="" class="fade-slide">
+    <img src="<?php echo esc_url(get_theme_file_uri('images/mainvisual3.jpg')); ?>" alt="" class="fade-slide">
+  </div>
+
+
+  <!-- New Arrival -->
+  <section id="arrival" class="arrival">
+    <h2 class="sec-title tween-animate-title">NEW ARRIVAL</h2>
+
+    <!-- swiper.js-->
+    <div class="swiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide"><img src="<?php echo esc_url(get_theme_file_uri('images/newarrival1.jpg')); ?>" alt="ピックアップ１ テキストテキスト"></div>
+        <div class="swiper-slide"><img src="<?php echo esc_url(get_theme_file_uri('images/newarrival2.jpg')); ?>" alt="ピックアップ１ テキストテキスト"></div>
+        <div class="swiper-slide"><img src="<?php echo esc_url(get_theme_file_uri('images/newarrival3.jpg')); ?>" alt="ピックアップ１ テキストテキスト"></div>
+        <div class="swiper-slide"><img src="<?php echo esc_url(get_theme_file_uri('images/newarrival4.jpg')); ?>" alt="ピックアップ１ テキストテキスト"></div>
+        <div class="swiper-slide"><img src="<?php echo esc_url(get_theme_file_uri('images/newarrival5.jpg')); ?>" alt="ピックアップ１ テキストテキスト"></div>
+        <div class="swiper-slide"><img src="<?php echo esc_url(get_theme_file_uri('images/newarrival6.jpg')); ?>" alt="ピックアップ１ テキストテキスト"></div>
+        <div class="swiper-slide"><img src="<?php echo esc_url(get_theme_file_uri('images/newarrival7.jpg')); ?>" alt="ピックアップ１ テキストテキスト"></div>
+        <div class="swiper-slide"><img src="<?php echo esc_url(get_theme_file_uri('images/newarrival8.jpg')); ?>" alt="ピックアップ１ テキストテキスト"></div>
+        <div class="swiper-slide"><img src="<?php echo esc_url(get_theme_file_uri('images/newarrival9.jpg')); ?>" alt="ピックアップ１ テキストテキスト"></div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Products -->
+  <section id="products" class="products">
+    <h2 class="sec-title tween-animate-title">PRODUCTS</h2>
+    <div class="products__grid">
+      <div class="products__item">
+        <a href="article1.html">
+          <div class="cover-slide hover-darken">
+            <img class="img-zoom" src="images/item1.jpg" alt="">
+          </div>
+          <div class="products__content">
+            <p class="products__category">category</p>
+            <p class="products__text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p class="products__date">XXXX.XX.XX</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="products__item">
+        <a href="article2.html">
+          <div class="cover-slide hover-darken">
+            <img class="img-zoom" src="images/item2.jpg" alt="">
+          </div>
+          <div class="products__content">
+            <p class="products__category">category</p>
+            <p class="products__text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p class="products__date">XXXX.XX.XX</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="products__item">
+        <a href="article3.html">
+          <div class="cover-slide hover-darken">
+            <img class="img-zoom" src="images/item3.jpg" alt="">
+          </div>
+          <div class="products__content">
+            <p class="products__category">category</p>
+            <p class="products__text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p class="products__date">XXXX.XX.XX</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="products__item">
+        <a href="article4.html">
+          <div class="cover-slide hover-darken">
+            <img class="img-zoom" src="images/item4.jpg" alt="">
+          </div>
+          <div class="products__content">
+            <p class="products__category">category</p>
+            <p class="products__text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p class="products__date">XXXX.XX.XX</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="products__item">
+        <a href="article5.html">
+          <div class="cover-slide hover-darken">
+            <img class="img-zoom" src="images/item5.jpg" alt="">
+          </div>
+          <div class="products__content">
+            <p class="products__category">category</p>
+            <p class="products__text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p class="products__date">XXXX.XX.XX</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="products__item">
+        <a href="article6.html">
+          <div class="cover-slide hover-darken">
+            <img class="img-zoom" src="images/item6.jpg" alt="">
+          </div>
+          <div class="products__content">
+            <p class="products__category">category</p>
+            <p class="products__text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p class="products__date">XXXX.XX.XX</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="products__item">
+        <a href="article7.html">
+          <div class="cover-slide hover-darken">
+            <img class="img-zoom" src="images/item7.jpg" alt="">
+          </div>
+          <div class="products__content">
+            <p class="products__category">category</p>
+            <p class="products__text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p class="products__date">XXXX.XX.XX</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="products__item">
+        <a href="article8.html">
+          <div class="cover-slide hover-darken">
+            <img class="img-zoom" src="images/item8.jpg" alt="">
+          </div>
+          <div class="products__content">
+            <p class="products__category">category</p>
+            <p class="products__text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p class="products__date">XXXX.XX.XX</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="products__item">
+        <a href="article9.html">
+          <div class="cover-slide hover-darken">
+            <img class="img-zoom" src="images/item9.jpg" alt="">
+          </div>
+          <div class="products__content">
+            <p class="products__category">category</p>
+            <p class="products__text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p class="products__date">XXXX.XX.XX</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="products__btn appear up">
+      <a href="products.html">
+        <button class="btn cover-3d item">
+          <span>
+            View More
+          </span>
+        </button>
+      </a>
+    </div>
+  </section>
+</main>
+
 <?php get_footer(); ?>
