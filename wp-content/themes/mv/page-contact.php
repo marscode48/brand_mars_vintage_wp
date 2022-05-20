1739cf00501bafe8b7aa159971df356ee9857e93
Adachi Masakazu <marscode48@gmail.com>
2022-05-20 14:05:01 +0900

    page-contact.phpを作成

---
+++ wp-content/themes/mv/page-contact.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main class="main">
+  <div class="contact appear up">
+
+    <h2 class="page-title item"><?php the_title(); ?></h2>
+
+    <div class="contact-form item">
+      <?php the_content(); ?>
+    </div>
+    
+    </div>
+  </div>
+</main> 
+
+<?php get_footer(); ?>
